247b7f56695409a18d1b0e881ce29a8aab8896f7
sivatejaswi27 <114803924+sivatejaswi27@users.noreply.github.com>
2024-07-23 11:49:30 +0530

    Delete src directory

--- src/main/java/com/telusko/ProductSpringWeb/ProductDB.java
+++
@@ -1,9 +0,0 @@
-package com.telusko.ProductSpringWeb;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductDB extends JpaRepository<Product, Integer> {
-    Product findByName(String name);
-}
